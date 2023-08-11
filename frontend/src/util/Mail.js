342c27f2791998c71e9ecb707439b6505f49997d
ebodnar1 <ebodnar@uwo.ca>
2023-08-11 17:28:57 -0600

    Moved mail credentials to secret file

--- frontend/src/util/Mail.js
+++ frontend/src/util/Mail.js
@@ -1,10 +1,11 @@
 import { WECCA_SOFTWARE_EMAIL } from "../config";
+import { MAIL_ACCOUNT } from "../secrets";
 
 export const sendMail = (mailTo, submittedBy, emailBody, setSubmitMessage, formType, successMsg, failureMsg) => {
     window.Email.send({
         Host : "smtp.elasticemail.com",
-        Username : "",
-        Password : "",
+        Username : MAIL_ACCOUNT.Username,
+        Password : MAIL_ACCOUNT.Password,
         To : [mailTo, WECCA_SOFTWARE_EMAIL],
         From : "wecca.software@gmail.com",
         Subject : `The ${formType} form has been submitted by ${submittedBy}`,
