d267d2856b04f2111039137d7570bc78daf4bb7c
ebodnar1 <ebodnar@uwo.ca>
2023-09-15 10:20:07 -0400

    Removed secret

--- backend/secret.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    URI: "mongodb+srv://wecca:Wecca2023@cluster0.yjihcmr.mongodb.net/?retryWrites=true&w=majority&useUnifiedTopology=false",
-    SECRET_TOKEN: "-KaPdSgVkYp3s6v9y$B&E)H@MbQeThWmZq4t7w!z%C*F-JaNdRfUjXn2r5u8x/A?D(G+KbPeShVkYp3s6v9y$B&E)H@McQfTjWnZq4t7w!z%C*F-JaNdRgUkXp2s5u8x"
-} 
